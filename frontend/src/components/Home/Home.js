6ed6adbfd8d2e89b785c28169d8559cbf2abdef2
ShawonSUSTSWE <53114714+ShawonSUSTSWE@users.noreply.github.com>
2022-07-14 21:50:31 +0600

    Frontend in progress

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Button variant="outline-primary">Log In</Button>
+    </div>
+  );
+};
+
+export default Home;
