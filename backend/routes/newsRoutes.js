89392935179ded87a1869ae1436124667ffed802
ShawonSUSTSWE <53114714+ShawonSUSTSWE@users.noreply.github.com>
2022-07-29 23:01:40 +0600

    Caching added

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const newsController = require("../controllers/newsController");
+const { cache } = require("../utils/servercache");
 const { authenticate } = require("../utils/authenticate");
 const { newsPublishValidator } = require("../utils/newsValidator");
 
 const router = express.Router();
-router.get("/", newsController.getAllNews);
+router.get("/", cache(10), newsController.getAllNews);
 router.get("/mynews", authenticate, newsController.getOwnNews);
 router.get("/:uuid", newsController.getNews);
 router.get("/user/:uuid", newsController.getNewsbyUser);
---
+++ backend/utils/servercache.js
@@ -0,0 +1,21 @@
+const mcache = require("memory-cache");
+
+exports.cache = (duration) => {
+  return (req, res, next) => {
+    const key = "_express_" + req.originalUrl || req.url;
+    const cachedBody = mcache.get(key);
+    if (cachedBody) {
+      res.json(cachedBody);
+      return;
+    } else {
+      res.sendResponse = res.json;
+      res.json = (body) => {
+        if (body.status !== "error") {
+          mcache.put(key, body, duration * 1000);
+        }
+        res.sendResponse(body);
+      };
+      next();
+    }
+  };
+};
